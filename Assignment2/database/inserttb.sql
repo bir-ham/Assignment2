1d125ecc8aef8c639fd32a442b68b4a01c521945
Birhanu Hailemariam <birhanuh@gmail.com>
2014-05-08 21:17:25 +0300

    Modified inserttb.sql.

--- Assignment2/database/inserttb.sql
+++ Assignment2/database/inserttb.sql
@@ -1,6 +1,6 @@
-insert into instance (id, status, name) values (1, 0, 'Tee kakku');
-insert into instance (id, status, name) values (2, 0, 'Siivoa lattia');
-insert into instance (id, status, name) values (3, 0, 'Kay kaupassa');
-insert into instance (id, status, name) values (4, 1, 'Maala seinat');
-insert into instance (id, status, name) values (5, 0, 'Korja poyra');
-insert into instance (id, status, name) values (6, 2, 'Lue kirja');
+insert into instance (id, status, task) values (1, 0, 'Tee kakku');
+insert into instance (id, status, task) values (2, 0, 'Siivoa lattia');
+insert into instance (id, status, task) values (3, 0, 'Kay kaupassa');
+insert into instance (id, status, task) values (4, 1, 'Maala seinat');
+insert into instance (id, status, task) values (5, 0, 'Korja poyra');
+insert into instance (id, status, task) values (6, 2, 'Lue kirja');
